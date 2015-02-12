23d670557790f976210590f155aa93c1b6ec4a00
tnummy <tim.nummy@luceous.com>
2015-02-11 20:11:08 -0500

    FIX cb gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-
+gem 'cb-api', require: 'cb'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
--- app/controllers/jobsearch_controller.rb
+++ app/controllers/jobsearch_controller.rb
@@ -1,10 +1,44 @@
 class JobsearchController < ApplicationController
+  before_action :set_job
+  before_action :set_location 
+
   def location
+
   end
 
   def job
+
   end
 
   def results
+  	  	if @location && @job 
+	  		results = Cb.job.search({ location:@location, jobtitle:@job })
+			response = results.response
+			response_job_search = response["ResponseJobSearch"]
+			errors = response_job_search["Errors"]
+			total_count = response_job_search["TotalCount"]
+			result_data = response_job_search["Results"]
+			real_result_data = result_data["JobSearchResult"]
+			@job_results = real_result_data
+	  	end 
+  end
+
+  private 
+  def set_job
+  	@job = params[:job]
+    if @job
+      session[:job] = @job
+    else
+      @job = session[:job]
+    end  
+  end
+
+  def set_location
+  	@location = params[:location]
+    if @location
+      session[:location] = @location
+    else
+      @location = session[:location]
+    end
   end
 end
